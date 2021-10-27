c6f94b074149d50ad78b540527c538e7ba34c84e
RachamallaSwathi <swathirachamalla77@gmail.com>
2021-10-27 07:45:31 -0700

    block 4

--- block-BJaadr/code/index.js
+++ block-BJaadr/code/index.js
@@ -2,34 +2,98 @@
 
 function countAllPeople() {
   // your code goes here
+  let totalPeople=got.houses.reduce((acc,cv)=>{
+    acc=acc+cv.people.length;
+    return acc; 
+  },0);
+  return totalPeople;
 }
 
 function peopleByHouses() {
   // your code goes here
+  let final={};
+  let people=got.houses.reduce((acc,cv)=>{
+    final[cv.name]=cv.people.length;
+    return final; 
+  },{});
+  return people;
 }
 
 function everyone() {
   // your code goes here
+  let peopleName=[];
+    got.houses.reduce((acc,cv)=>{
+        acc=cv.people.reduce((accName,cvName)=>{
+         
+        accName=cvName.name
+       
+        peopleName=peopleName.concat(accName);
+        return accName;
+        
+         },'');
+        
+         
+         
+        return acc; 
+  },'')
+ return peopleName;
+  
 }
 
 function nameWithS() {
   // your code goes here
+  let nameIncludesS=[];
+   nameIncludesS=everyone().filter(n=>{
+     if(n.includes('S')||n.includes('s') ){
+       return n;
+     }
+   });
+   return nameIncludesS;
 }
 
 function nameWithA() {
   // your code goes here
+  let nameIncludesA=[];
+  nameIncludesA=everyone().filter(n=>{
+    if(n.includes('A')||n.includes('a') ){
+      return n;
+    }
+  });
+  return nameIncludesA;
 }
 
 function surnameWithS() {
   // your code goes here
+  let surNameStartsWithS=[];
+  surNameStartsWithS=everyone().filter(n=>{
+   if(n.includes('S',1)){
+      return n;
+     
+   }
+  })
+  return surNameStartsWithS;
 }
 
 function surnameWithA() {
   // your code goes here
+  let surNameStartsWithA=[];
+  surNameStartsWithA=everyone().filter(n=>{
+   if(n.includes('A',1)){
+      return n;
+     
+   }
+  })
+  return surNameStartsWithA;
 }
 
 function peopleNameOfAllHouses() {
   // your code goes here
+  let final={};
+  let people=got.houses.reduce((acc,cv)=>{
+    final[cv.name] =[cv.people.map(n=>n.name)];
+    return final; 
+  },{});
+  return people;
 }
 
 // Testing your result after writing your function
@@ -56,10 +120,10 @@ console.log(surnameWithS(), 'surname with s');
 // Output should be
 // ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow", "Catelyn Stark"]
 
-console.log(surnameWithA());
+console.log(surnameWithA(),'surname with a');
 // Output should be
 // ["Lysa Arryn", "Jon Arryn"]
 
 console.log(peopleNameOfAllHouses());
 // Output should be
-// {Arryns: ["Jon Arryn"], Baratheons: ["Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon"], Dothrakis: ["Khal Drogo"], Freys: ["Walder Frey"], Greyjoys: ["Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy"], Lannisters: ["Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon"], Redwyne: ["Olenna Tyrell"], Starks: ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow"], Targaryens: ["Daenerys Targaryen", "Viserys Targaryen"], Tullys: ["Catelyn Stark", "Lysa Arryn", "Edmure Tully", "Brynden Tully"], Tyrells: ["Margaery Baratheon", "Loras Tyrell"]}
+// {Arryns: ["Jon Arryn"], Baratheons: ["Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon"], Dothrakis: ["Khal Drogo"], Freys: ["Walder Frey"], Greyjoys: ["Balon Greyjoy", "Theon Greyjoy", "Yara Greyjoy"], Lannisters: ["Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon"], Redwyne: ["Olenna Tyrell"], Starks: ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Jon Snow"], Targaryens: ["Daenerys Targaryen", "Viserys Targaryen"], Tullys: ["Catelyn Stark", "Lysa Arryn", "Edmure Tully", "Brynden Tully"], Tyrells: ["Margaery Baratheon", "Loras Tyrell"]}'
